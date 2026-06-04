038f862ee17d2f5f26229ececb4037746f1ff9ca
Mayank Patel <91795753+MayankPatel7@users.noreply.github.com>
2026-06-04 20:54:45 +0530

    edited the vector2d header

--- src/Vector2D.h
+++ src/Vector2D.h
@@ -9,19 +9,19 @@ struct Vector2D
     Vector2D() : x(0.0f), y(0.0f) {}
     Vector2D(float x, float y) : x(x), y(y) {}
 
-    Vector2D operator+(const Vector2D& other){
+    Vector2D operator+(const Vector2D& other) const{
         return Vector2D(x + other.x, y + other.y);
     }
 
-    Vector2D operator-(const Vector2D& other){
+    Vector2D operator-(const Vector2D& other) const{
         return Vector2D(x - other.x, y - other.y);
     }
 
-    Vector2D operator*(const float scalar){
+    Vector2D operator*(const float scalar) const{
         return Vector2D(x*scalar, y*scalar); 
     }
 
-    Vector2D operator/(const float scalar){
+    Vector2D operator/(const float scalar) const{
         return Vector2D(x/scalar, y/scalar); 
     }
 
